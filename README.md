3fd57928004564c2749e26e678efd25726e41859
Timothy Stiles <tim@stiles.io>
2018-03-15 22:39:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# supergal
+Super Get a License - generates licenses from the command line.
+
+
+## installation
+
+```
+npm install -g supergal
+```
+
+or
+
+```
+yarn install supergal
+```
+
+## usage
+
+![Usage gif](resources/usage.gif)
+```
+supergal <license> 
+```
+
+For example if you wanted to use the MIT license:
+```
+supergal mit
+```
+
+## building
+
+To build your own local version do the following.
+
+```
+git clone 
+```
+```
+cd supergal
+```
+```
+npm run build
+```
+## contributing
+Please make a fork, and branch to work on new features. Pull request to merge.
+
+Feel free to add a template for your favorite license, add documentation, templating for custom names, pretty up the command line interface, add tests, or whatever you want! All pull requests considered.
+
+## license
+
+Copyright 2018 Timothy Stiles
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const fs = require('fs');
+
+program
+    .arguments('<license>')
+    .action(function(license) {
+        switch (license) {
+            case 'agpl3':
+                setLicense('/licenses/agpl-3.0.txt');
+                break;
+            case 'ap2':
+                setLicense('/licenses/apache-license-2.0.txt');
+                break;
+            case 'bsd2':
+                setLicense('/licenses/bsd-2-clause.txt');
+                break;
+            case 'bsd3':
+                setLicense('/licenses/bsd-3-clause.txt');
+                break;
+            case 'gpl2':
+                setLicense('/licenses/gpl-2.0.txt');
+                break;
+            case 'gpl3':
+                setLicense('/licenses/gpl-3.0.txt');
+                break;
+            case 'lgpl2':
+                setLicense('/licenses/lgpl-2.0.txt');
+                break;
+            case 'lgpl3':
+                setLicense('/licenses/lgpl-3.0.txt');
+                break;
+            case 'mit':
+                setLicense('/licenses/mit.txt');
+                break;
+            default:
+                console.log('No license with that name.')
+        }
+    })
+    .parse(process.argv);
+
+
+function setLicense(license) {
+    fs.copyFile(__dirname + license, "LICENSE", (err) => {
+        if (err) throw err;
+    })
+}
